8a8b510a740723bd285255cacfcfc683e55483e9
Tobias Bück <youremail@yourdomain.com>
2021-05-02 13:24:58 +0200

    intitial commit

---
+++ cmake-build-debug/CMakeFiles/PAD2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAD2.dir/main.cpp.o"
+  "PAD2"
+  "PAD2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PAD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PAD2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PAD2.
+# This may be replaced when dependencies are built.
